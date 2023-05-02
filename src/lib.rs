115dabb6e56a3533baa34f3a7852890644d34e1d
Danny Rakita <djrakita@gmail.com>
2023-05-02 10:25:03 -0400

    tested alias type

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,110 @@
 use std::fmt::{Debug, Display};
-use std::ops::{Add, Div, Mul, Rem, Sub};
 use nalgebra::{Dim, Matrix, RawStorageMut};
 use simba::scalar::ComplexField;
 
 pub trait AD :
-    Add<f64, Output=Self> +
-    Mul<f64, Output=Self> +
-    Sub<f64, Output=Self> +
-    Div<f64, Output=Self> +
-    Rem<f64, Output=Self> +
     ComplexField +
     PartialOrd +
     PartialEq +
     Debug +
     Display
 {
-    fn constant(v: f64) -> Self;
-    fn scalar_multiply_by_nalgebra_matrix<R: Dim + Clone, C: Dim + Clone, S: Clone + RawStorageMut<Self, R, C>>( scalar: f64, matrix: &Matrix<Self, R, C, S> ) -> Matrix<Self, R, C, S>;
+    fn constant(constant: f64) -> Self;
+    fn scalar_add(arg1: f64, arg2: Self) -> Self;
+    fn scalar_lsub(arg1: f64, arg2: Self) -> Self;
+    fn scalar_rsub(arg1: Self, arg2: f64) -> Self;
+    fn scalar_mul(arg1: f64, arg2: Self) -> Self;
+    fn scalar_ldiv(arg1: f64, arg2: Self) -> Self;
+    fn scalar_rdiv(arg1: Self, arg2: f64) -> Self;
+    fn scalar_lrem(arg1: f64, arg2: Self) -> Self;
+    fn scalar_rrem(arg1: Self, arg2: f64) -> Self;
+    fn scalar_mul_by_nalgebra_matrix<R: Dim + Clone, C: Dim + Clone, S: Clone + RawStorageMut<Self, R, C>>(scalar: f64, matrix: &Matrix<Self, R, C, S> ) -> Matrix<Self, R, C, S>;
 }
-
 
 impl AD for f64 {
     fn constant(v: f64) -> Self {
         return v;
     }
-    fn scalar_multiply_by_nalgebra_matrix<R: Dim + Clone, C: Dim + Clone, S: Clone + RawStorageMut<Self, R, C>>(scalar: f64, matrix: &Matrix<Self, R, C, S>) -> Matrix<Self, R, C, S> {
+
+    fn scalar_add(arg1: f64, arg2: Self) -> Self {
+        arg1 + arg2
+    }
+
+    fn scalar_lsub(arg1: f64, arg2: Self) -> Self {
+        arg1 - arg2
+    }
+
+    fn scalar_rsub(arg1: Self, arg2: f64) -> Self {
+        arg1 - arg2
+    }
+
+    fn scalar_mul(arg1: f64, arg2: Self) -> Self {
+        arg1 * arg2
+    }
+
+    fn scalar_ldiv(arg1: f64, arg2: Self) -> Self {
+        arg1 / arg2
+    }
+
+    fn scalar_rdiv(arg1: Self, arg2: f64) -> Self {
+        arg1 / arg2
+    }
+
+    fn scalar_lrem(arg1: f64, arg2: Self) -> Self {
+        arg1 % arg2
+    }
+
+    fn scalar_rrem(arg1: Self, arg2: f64) -> Self {
+        arg1 % arg2
+    }
+
+    fn scalar_mul_by_nalgebra_matrix<R: Dim + Clone, C: Dim + Clone, S: Clone + RawStorageMut<Self, R, C>>(scalar: f64, matrix: &Matrix<Self, R, C, S>) -> Matrix<Self, R, C, S> {
         let mut out = matrix.clone();
         out.iter_mut().for_each(|x| *x*=scalar );
+        out
+    }
+}
+
+impl AD for f32 {
+    fn constant(v: f64) -> Self {
+        return v as f32;
+    }
+
+    fn scalar_add(arg1: f64, arg2: Self) -> Self {
+        arg1 as f32 + arg2
+    }
+
+    fn scalar_lsub(arg1: f64, arg2: Self) -> Self {
+        arg1 as f32 - arg2
+    }
+
+    fn scalar_rsub(arg1: Self, arg2: f64) -> Self {
+        arg1 - arg2 as f32
+    }
+
+    fn scalar_mul(arg1: f64, arg2: Self) -> Self {
+        arg1 as f32 * arg2
+    }
+
+    fn scalar_ldiv(arg1: f64, arg2: Self) -> Self {
+        arg1 as f32 / arg2
+    }
+
+    fn scalar_rdiv(arg1: Self, arg2: f64) -> Self {
+        arg1 / arg2 as f32
+    }
+
+    fn scalar_lrem(arg1: f64, arg2: Self) -> Self {
+        arg1 as f32 % arg2
+    }
+
+    fn scalar_rrem(arg1: Self, arg2: f64) -> Self {
+        arg1 % arg2 as f32
+    }
+
+    fn scalar_mul_by_nalgebra_matrix<R: Dim + Clone, C: Dim + Clone, S: Clone + RawStorageMut<Self, R, C>>(scalar: f64, matrix: &Matrix<Self, R, C, S>) -> Matrix<Self, R, C, S> {
+        let mut out = matrix.clone();
+        out.iter_mut().for_each(|x| *x*=scalar as f32 );
         out
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
-use nalgebra::{DVector, Vector3};
+#![feature(type_alias_impl_trait)]
+
 use ad_trait::AD;
 
-pub fn test<F: AD>(a: F, b: F) -> F {
-    a * b
-}
+pub type T = impl AD;
 
-pub fn test2<F: AD>(a: DVector<F>, b: DVector<F>) -> DVector<F> {
-    return F::scalar_multiply_by_nalgebra_matrix(3.0, &a) + b;
+pub fn test(a: T, b: T) -> T {
+    a + b
 }
 
 fn main() {
-    let res = test2::<f64>(DVector::from_vec(vec![1.,2.]), DVector::from_vec(vec![3.,4.]));
+    let res = test(1.0 as f32, 2.0);
     println!("{:?}", res);
 
-    let res = test(1., 2.);
+    let res = test(1.0, 2.0);
     println!("{:?}", res);
-
-    let v = Vector3::new(1.,2.,3.);
-    let vv = f64::scalar_multiply_by_nalgebra_matrix(3.0, &v);
-    println!("{:?}", vv);
 }
